ba374413444867c4aa254d41fcf95046d6bcad7a
Chandler Casey <chandlercasey13@gmail.com>
2024-07-09 22:18:13 -0500

    merged server and client projects into one project

--- client/src/components/TextInput.jsx
+++ client/src/components/TextInput.jsx
@@ -2,23 +2,29 @@ import React, { useState } from "react";
 import { socket } from '../socket';
 
 
-function TextInput({handleButtonSubmit}) {
+function TextInput() {
 
 
-const [textInputData, setTextInputData] = useState('')
+const [textInputData, setTextInputData] = useState({textInput: ''})
+const [messageLog, setMessageLog] = useState([])
+
+
 
 function handleTextInput (event) {
 
 setTextInputData({...textInputData, [event.target.name]: event.target.value })
+console.log(textInputData)
 }
 
 function handleButtonSubmit(e) {
     e.preventDefault();
     
-    socket.emit('message', 'world')
+    socket.emit('message', textInputData.textInput)
+    setTextInputData({textInput: ''})
     
     }
 
+    console.log(textInputData.textInput)
 return (
 
     <>
@@ -35,7 +41,7 @@ return (
 
 <label htmlFor="textInput"></label>
 
-<input id= 'textInput' name = 'textInput' type="text" onChange={handleTextInput} value={textInputData.TextInput}></input>
+<input id= 'textInput' name = 'textInput' type="text" value={textInputData.textInput} onChange={handleTextInput} ></input>
 
     <button> <i className='bx bxs-send'></i></button>
     </form>
